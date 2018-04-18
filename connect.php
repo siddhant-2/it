1121766a189f8f703fc5c1e30a30cc13c6015506
siddhant-2 <sidsiddhant2@gmail.com>
2018-04-18 09:52:03 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$db_host="localhost";
+$db_user="root";
+$db_password="tiger";
+$db_name="2015cse133";
+$dbh = mysqli_connect($db_host,$db_user,$db_password,$db_name) or die ("could not connect to database".mysqli_error($dbh));
+
+?>
